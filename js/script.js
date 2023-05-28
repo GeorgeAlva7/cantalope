4350b68df62512f2597db3bef8c8d206cc44411e
GeorgeAlva7 <98796254+GeorgeAlva7@users.noreply.github.com>
2023-05-28 13:59:40 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,186 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.container-fluid', {
+        duration: 800,
+        origin: 'bottom',
+        distance: '-400px'
+    });
+    
+
+    sr.reveal('.presentacion .h1',{
+        duration: 1200,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+    
+    sr.reveal('.presentacion .h5',{
+        delay: 1000,
+        duration: 1000,
+        origin: 'bottom',
+        distance: '-40px'
+    });
+
+    sr.reveal('.presentacion .h4',{
+        delay: 1000,
+        duration: 1000,
+        origin: 'bottom',
+        distance: '-40px'
+    });
+
+    sr.reveal('.contenedor', {
+        duration: 800,
+        delay: 200
+    });
+
+    //SOBRE MI
+    sr.reveal('.subtitulo1', {
+        duration: 1500,
+        delay: 200
+    });
+    sr.reveal('.subcontenedor1', {
+        duration: 1500,
+        delay: 200
+    });
+    sr.reveal('.foto1', {
+        duration: 1500,
+        delay: 200
+    });
+
+    //LOGROS
+    sr.reveal('.subtitulo2', {
+        duration: 1500,
+        delay: 200
+    });
+    sr.reveal('.foto2', {
+        duration: 1500,
+        delay: 200
+    });
+    sr.reveal('.subcontenedor2', {
+        duration: 1500,
+        delay: 200
+    });
+
+    //PROYECTOS
+    sr.reveal('.subtitulo3', {
+        duration: 1500,
+        delay: 200
+    });
+    sr.reveal('.subcontenedor3', {
+        duration: 1500,
+        delay: 200
+    });
+    sr.reveal('.foto3', {
+        duration: 1500,
+        delay: 200
+    });
+    
+    sr.reveal('.foto4', {
+        duration: 1500,
+        delay: 200
+    });
+    sr.reveal('.subcontenedor4', {
+        duration: 1500,
+        delay: 200
+    });
+
+    
+    sr.reveal('.subcontenedor5', {
+        duration: 1500,
+        delay: 200
+    });
+    sr.reveal('.foto5', {
+        duration: 1500,
+        delay: 200
+    });
+
+    
+    sr.reveal('.foto6', {
+        duration: 1500,
+        delay: 200
+    });
+    sr.reveal('.subcontenedor6', {
+        duration: 1500,
+        delay: 200
+    });
+
+    
+    sr.reveal('.subcontenedor7', {
+        duration: 1500,
+        delay: 200
+    });
+    sr.reveal('.foto7', {
+        duration: 1500,
+        delay: 200
+    });
+    
+    sr.reveal('.foto8', {
+        duration: 1500,
+        delay: 200
+    });
+    sr.reveal('.subcontenedor8', {
+        duration: 1500,
+        delay: 200
+    });
+
+    //CONTACTO
+    sr.reveal('.subtitulo4', {
+        duration: 1500,
+        delay: 200
+    });
+    sr.reveal('.container', {
+        duration: 1500,
+        delay: 200
+    });
+
+    // SOBREMI.HMTL y  LOGROS.HTML 
+
+    sr.reveal('.titulo',{
+        delay: 500,
+        duration: 500,
+        origin: 'left',
+        distance: '160px'
+    });
+
+    sr.reveal('div .contenedor-b .center', {
+        delay: 700,
+        duration: 1500,
+        origin: 'left',
+        distance: '160px'
+    });
+
+    // PROYECTOS.HTML
+
+    sr.reveal('.tit .titulo',{
+        delay: 0,
+        duration: 1200,
+        origin: 'left',
+        distance: '200px'
+    });
+    
+    sr.reveal('.galeria1 div center',{
+        delay: 200,
+        duration: 1200,
+        origin: 'left',
+        distance: '200px'
+    })
+    
+    sr.reveal('.galeria1 .carousel',{
+        delay: 500,
+        duration: 1200,
+        origin: 'left',
+        distance: '200px'
+    })
+
+    sr.reveal('.galeria2 div center',{
+        duration: 500,
+        origin: 'left',
+        distance: '200px'
+    })
+    
+    sr.reveal('.galeria2 .carousel',{
+        delay: 200,
+        duration: 1200,
+        origin: 'left',
+        distance: '200px'
+    })
+    
